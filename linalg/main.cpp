d9f4e9e97e7a65b1c4eef61b7fd334b6ff9ace67
hiddevuijk <hiddevuijk@hiddevuijk-ThinkPad-T400>
2018-01-21 11:07:17 +0100

    changed main to check changes to tranpose

--- linalg/main.cpp
+++ linalg/main.cpp
@@ -13,22 +13,19 @@ using namespace std;
 
 int main()
 {
-	Matd m(3,3,0);
+	Matd m(2,3,0);
 	m(0,0) = 5;
 	m(0,1) = -.1;
 	m(0,2) = -.2;
 	m(1,0) = .1;
 	m(1,1) = 7;
 	m(1,2) = -.3;
-	m(2,0) = .3;
-	m(2,1) = -.2;
-	m(2,2) = 10;
-	m.print(cout);
 	cout << endl;
-	m.T();
 	m.print(cout);
-	Matd m2 = T(T(m));
+	cout << endl << m.cols() << endl << m.rows() << endl <<endl;
+	Matd m2 = transpose(m);
 	cout<< endl;
 	m2.print(cout);
+	cout << endl << m2.cols() << endl << m2.rows() << endl <<endl;
 	return 0;
 }
